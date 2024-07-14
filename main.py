4cd57d41b503c4df75ae3045b6cce2b7fc59a676
aondrej ryan <aondrejryan11@gmail.com>
2024-07-13 23:17:43 -0700

    Commit message with custom date

--- main.py
+++ main.py
@@ -80,3 +80,14 @@ class PracticingPythonOnTheBackend(BaseHTTPRequestHandler):
         self.end_headers()
         response = {'message': 'This is a GET request'}
         self.wfile.write(json.dumps(response).encode())
+
+
+def run(server_class=HTTPServer, handler_class=PracticingPythonOnTheBackend, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on  {port}...')
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    run()
